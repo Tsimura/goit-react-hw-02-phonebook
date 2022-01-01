8ab07fa49fec6004f2c33e95b325ce198522fa89
Tsimura <chappa2828@gmail.com>
2022-01-01 23:14:40 +0200

    in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
-import React from 'react';
-function App() {
-  return <p>Phonebook</p>;
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <>
+        <h1> Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+      </>
+    );
+  }
 }
 
 export default App;
